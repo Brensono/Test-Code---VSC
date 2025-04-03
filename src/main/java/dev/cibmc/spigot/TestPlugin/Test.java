5772dc791546167f860d91543b00d152cf79053c
Brensono <Benjamin.Metz.2011@gmail.com>
2025-04-03 19:06:31 +0200

    This still is a Test!

--- src/main/java/dev/cibmc/spigot/TestPlugin/Test.java
+++
@@ -1,5 +0,0 @@
-package src.main.java.dev.cibmc.spigot.TestPlugin;
-
-public class Test {
-    
-}
--- src/main/java/io/papermc/testplugin/Test.java
+++ src/main/java/io/papermc/testplugin/Test.java
@@ -1,5 +1,13 @@
 package src.main.java.io.papermc.testplugin;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public class Test {
-    
+public class Test extends JavaPlugin{
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello, SpigotMC!");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("See you again, SpigotMC!");
+    }
 }
