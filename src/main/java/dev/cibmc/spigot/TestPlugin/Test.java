6c6e713bc9967c1b6fe870293a0abe210ce226ae
Brensono <Benjamin.Metz.2011@gmail.com>
2025-04-03 18:52:15 +0200

    New files I guess

---
+++ src/main/java/dev/cibmc/spigot/TestPlugin/Test.java
@@ -0,0 +1,5 @@
+package src.main.java.dev.cibmc.spigot.TestPlugin;
+
+public class Test {
+    
+}
---
+++ src/main/java/io/github/Brensono/TestPlugin/Test.java
@@ -0,0 +1,5 @@
+package src.main.java.io.github.Brensono.TestPlugin;
+
+public class Test {
+    
+}
