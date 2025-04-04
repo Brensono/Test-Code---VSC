f9a5b12a39575d4569d0a259ccd72ba8d1208461
Brensono <Benjamin.Metz.2011@gmail.com>
2025-04-04 14:26:35 +0200

    I modified some Code and I'm hoping that JavaPlugin will work

--- src/main/java/io/github/Brensono/TestPlugin/Test.java
+++ src/main/java/io/github/Brensono/TestPlugin/Test.java
@@ -1,4 +1,9 @@
 package src.main.java.io.github.Brensono.TestPlugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Test extends JavaPlugin{
--- src/main/java/io/papermc/testplugin/Test.java
+++ src/main/java/io/papermc/testplugin/Test.java
@@ -1,7 +1,13 @@
 package src.main.java.io.papermc.testplugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Test extends JavaPlugin{
+public class Test extends JavaPlugin implements Listener{
+    
     @Override
     public void onEnable() {
         getLogger().info("Hello, SpigotMC!");
