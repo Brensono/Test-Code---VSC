5f3464a58dcbeba057b5b9a94bbfd9a5448e8346
Brensono <Benjamin.Metz.2011@gmail.com>
2025-04-06 16:15:37 +0200

    NICE Code

--- src/main/java/io/papermc/testplugin/JavaPlugin.java
+++
@@ -1,5 +0,0 @@
-package src.main.java.io.papermc.testplugin;
-
-public class JavaPlugin {
-
-}
--- src/main/java/io/papermc/testplugin/Test.java
+++ src/main/java/io/papermc/testplugin/Test.java
@@ -1,6 +1,7 @@
-package src.main.java.io.papermc.testplugin;
+package io.papermc.testplugin;
 
 import java.lang.Object;
+import net.kyori.adventure.text.Component;
 import org.bukkit.plugin.PluginBase;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
@@ -11,15 +12,22 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Test extends JavaPlugin implements Listener{
 
+public class Test extends JavaPlugin implements Listener{
+    
     @Override
     public void onEnable() {
         getLogger().info("Hello, PaperMC!");
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
     @Override
     public void onDisable() {
         getLogger().info("See you again, PaperMC!");
     }
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage(("Hello, " + event.getPlayer().getName() + "!"));
+    }
 }
+
 
